a68cf658b4c6784205c1dec948b272f5ca80bf5a
chrisb3546 <chrisbradycode@gmail.com>
2020-12-09 13:39:34 -0500

    add rotation functionality too player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,19 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float xSpeed = 50f;
+    [SerializeField] float Speed = 50f;
+
     [SerializeField] float xRange = 22f;
+    [SerializeField] float yRange = 14f;
+    [SerializeField] float positionPitchRatio = -2f;
+    [SerializeField] float controlledPitchRatio = -10f;
+    [SerializeField] float positionRollRatio = -2f;
+    [SerializeField] float positionYawRatio = 1.5f;
+    [SerializeField] float controlledRollRatio = -10f;
+
+    float xThrow, yThrow;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +27,32 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffset = xThrow * xSpeed * Time.deltaTime;
+     ProcessTranslation();
+     ProcessRotation();
+
+    }
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * Speed * Time.deltaTime;
         float rawXPos = transform.localPosition.x + xOffset;
         float clampedXpos = Mathf.Clamp(rawXPos, -xRange, xRange);
-
         transform.localPosition = new Vector3(clampedXpos, transform.localPosition.y, transform.localPosition.z);
         
+
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * Speed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos =Mathf.Clamp(rawYPos, -yRange, yRange);
+        // float clampedXpos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        transform.localPosition = new Vector3(transform.localPosition.x, clampedYPos, transform.localPosition.z);
+    }
+
+    private void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * positionPitchRatio + yThrow * controlledPitchRatio;
+        float yaw = transform.localPosition.x * positionYawRatio;
+        float roll = transform.localPosition.x * positionRollRatio + xThrow * controlledRollRatio;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
