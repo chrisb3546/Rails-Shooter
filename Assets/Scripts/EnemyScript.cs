dd1eeb7d3b23b40e3e2503c4d1f8082d3ab1819e
chrisb3546 <chrisbradycode@gmail.com>
2020-12-11 12:48:08 -0500

    Add enemy script to control particle collision functionality(explosion, death)

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    [SerializeField] GameObject explosion;
+    // [SerializeField] GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+     void OnParticleCollision(GameObject other)
+    {
+        explosion.SetActive(true);
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
